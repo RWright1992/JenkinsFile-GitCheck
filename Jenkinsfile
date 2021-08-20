f61038ebb9d144a87e3a97159f62ff3ac9b6a77a
RWright1992 <ryanwright1992@gmail.com>
2021-08-20 09:50:27 +0100

    adding gitcheck script

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+pipeline{
+	agent any
+	stages{
+		stage('Check if git repo exists'){
+		steps{
+			sh 'bash gitcheck.sh'
+			}
+		}
+	}
+
+}
---
+++ gitcheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+
+REPO=SIFA-Project-1
+
+if [! -d "$REPO"]; then
+	git clone https://github.com/RWright1992/SIFA-Project-1.git
+else
+	git fetch https://github.com/RWright1992/SIFA-Project-1.git
