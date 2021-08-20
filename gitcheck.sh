75780fbbcb3ac163f12e34f55075a97f2dd7dfb0
RWright1992 <ryanwright1992@gmail.com>
2021-08-20 09:55:40 +0100

    adding gitcheck script

--- gitcheck.sh
+++ gitcheck.sh
@@ -2,7 +2,7 @@
 
 REPO=SIFA-Project-1
 
-if [! -d "$REPO"]; then
+if [ ! -d "$REPO" ]; then
 	git clone https://github.com/RWright1992/SIFA-Project-1.git
 else
 	git fetch https://github.com/RWright1992/SIFA-Project-1.git
